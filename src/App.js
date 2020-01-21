2deb931dd79256c9586c8a37c59343072bec48d0
Glen Turnbow <glenturnbow@gmail.com>
2020-01-20 22:36:43 -0700

    added imgAssets components folders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './imgAssets/GRF_logo_2.png';
+// import logo from './logo.svg';
 import './App.css';
+import MyNewComponent from './components/MyNewComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MyNewComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
