bcf2de4a3943809728519d33bc276a278177e91a
Glen Turnbow <glenturnbow@gmail.com>
2020-05-02 17:07:40 -0600

    test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './imgAssets/GRF_logo_2.png';
 // import logo from './logo.svg';
 import './App.css';
 import MyNewComponent from './components/MyNewComponent';
+import HomeComponent from './components/Home'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <MyNewComponent />
+        <HomeComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
