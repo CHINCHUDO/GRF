376c0a6273b860073c3c99aa7abf4e9f317b1fff
Glen Turnbow <glenturnbow@gmail.com>
2021-01-28 01:42:59 -0700

    Updating npm packages etcetera

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import GRFTitle from './GRFTitleComponent';
-// import logo from './imgAssets/GRF_logo_2.png';
-// import logo from './logo.svg';
+import logo from '../imgAssets/GRF_logo_2.png';
+// import logo from '../logo.svg';
 
 
 const HomeComponent = () => (
-<div class="App-pages">
+<div class="App-pages">   
+    <img  src={logo} className="Main-Page-Logo" alt="logo" />
     <h1><GRFTitle/></h1>
     <p>Welcome to the home page. Please wait while we work on this site and apply another coat of sunscreen. </p>
 </div>)
